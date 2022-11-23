b8f681ea2bfb4287f3ac235a65d82d2389c0cdde
Grant Bradley <grantbradleytiger13@gmail.com>
2022-11-23 17:16:54 -0500

    Initial Commit

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car dodge = new Car();    
+            Car tesla = new Car();
+            Car myCar = new Car();
+            myCar.Make = "Toyota";
+            myCar.Model = "Camry";
+            myCar.Year = 2011;
+
+            tesla.Make = "Tesla";
+            tesla.Model = "Sport";
+            tesla.Year = 2022;
+
+            dodge.Make = "Dodge";
+            dodge.Model = "Charger";
+            dodge.Year = 2016;
+
+            var carList = new List<Car>() { myCar, tesla, dodge};
+
+            foreach(var car in carList)
+            {
+                Console.WriteLine($"{car.Make} {car.Model} {car.Year}");
+            }
         }
     }
 }
